9347827ac2b66c58c6598b9c9d1be38f9190ffcb
Aman kumar <iit2022168@iiita.ac.in>
2025-07-04 20:39:11 +0530

    hii

--- index.js
+++ index.js
@@ -16,8 +16,14 @@ const aman = (req,resp)=>{
       resp.write("<h1>Hello heuuu this is aman kumar</h1>");
       resp.end();
 }
+const aman1 = (req,resp)=>{
 
+      resp.write("<h1>Hello heuuu this is aman kumar</h1>");
+      resp.end();
+}
 http.createServer(aman).listen(4000);
 
 console.log("serveer is started on 4000")
 console.log("serveer is started on 4000")
+console.log("serveer is started on 4000")
+console.log("serveer ison 4000")
